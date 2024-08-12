2170d55bc2650d36462bf422554e167d51328736
India Jackson, PhD <ijackson1@gsu.edu>
2024-08-12 12:31:04 -0400

    Create run_predict_save.sh

---
+++ run_predict_save.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change to the directory where the Python script is located
+cd /home/indiajacksonphd/scripts/
+
+# Run the Python script using the Python interpreter from the Conda environment
+/home/indiajacksonphd/anaconda3/envs/uncertaintyenv/bin/python3 predict_save.py
