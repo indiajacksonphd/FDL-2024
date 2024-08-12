56cac47c2341d13dfff67a36e40376fb1d21e8cb
India Jackson, PhD <ijackson1@gsu.edu>
2024-08-12 12:30:44 -0400

    Update download_scripts.sh

--- download_scripts.sh
+++ download_scripts.sh
@@ -6,14 +6,18 @@ BASE_URL="https://raw.githubusercontent.com/indiajacksonphd/FDL-2024/main"
 # Download each script using curl
 echo "Downloading train_model.py..."
 curl -o train_model.py $BASE_URL/train_model.py
+chmod +x train_model.py
 
 echo "Downloading save_outputs.py..."
 curl -o save_outputs.py $BASE_URL/save_outputs.py
+chmod +x save_outputs.py
 
 echo "Downloading predict_save.py..."
 curl -o predict_save.py $BASE_URL/predict_save.py
+chmod +x predict_save.py
 
 echo "Downloading generate_data.py..."
 curl -o generate_data.py $BASE_URL/generate_data.py
+chmod +x generate_data.py
 
 echo "Download complete."
