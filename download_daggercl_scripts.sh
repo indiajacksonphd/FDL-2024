5238cd89201a8522919e756e6da6261fdf5f4cae
India Jackson, PhD <ijackson1@gsu.edu>
2024-08-14 10:48:33 -0400

    Update download_daggercl_scripts.sh

--- download_daggercl_scripts.sh
+++ download_daggercl_scripts.sh
@@ -4,16 +4,20 @@
 BASE_URL="https://raw.githubusercontent.com/indiajacksonphd/FDL-2024/main"
 
 # Download each script using curl
-echo "Downloading train_model.py..."
+echo "Downloading supermag_UQ_OI_IJ.py..."
 curl -o supermag_UQ_OI_IJ.py $BASE_URL/supermag_UQ_OI_IJ.py
 chmod +x supermag_UQ_OI_IJ.py
 
-echo "Downloading save_outputs.py..."
+echo "Downloading sec.py..."
 curl -o sec.py $BASE_URL/sec.py
 chmod +x sec.py
 
+echo "Downloading train_model.py..."
+curl -o supermag_api.py $BASE_URL/supermag_api.py
+chmod +x supermag_api.py
+
 echo "Downloading save_outputs.py..."
-curl -o run_supermag_5m.py $BASE_URL/run_supermag_5m.sh
-chmod +x run_supermag_5m.py
+curl -o run_supermag_5m.sh $BASE_URL/run_supermag_5m.sh
+chmod +x run_supermag_5m.sh
 
 echo "Download complete."
