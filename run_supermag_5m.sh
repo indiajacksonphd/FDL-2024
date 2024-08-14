123b88656f1c7596e22b4bcdc3b0b50b4b509693
India Jackson, PhD <ijackson1@gsu.edu>
2024-08-14 09:45:04 -0400

    Create run_supermag_5m.sh

---
+++ run_supermag_5m.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change to the directory where the Python script is located
+cd /home/indiajacksonphd/scripts/uncertainty/
+
+# Run the Python script using the Python interpreter from the Conda environment
+/home/indiajacksonphd/anaconda3/envs/uncertaintyenv/bin/python3 supermag_UQ_OI_IJ.py
